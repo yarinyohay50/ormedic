f6ad0806b9d7c9d4db1ab117d2c4de443ce0563d
yarinyohay50 <yarinyohay50@gmail.com>
2026-05-23 23:48:34 +0300

    Delete supabase directory

--- supabase/migrations/20260516202039_7ff31284-3600-4e22-b222-56fbe8338f9b.sql
+++
@@ -1,5 +0,0 @@
-UPDATE customers
-SET customer_follow_up = NULL
-WHERE customer_follow_up IS NOT NULL
-  AND purchase_date IS NOT NULL
-  AND customer_follow_up = (date_trunc('month', purchase_date) + interval '1 month')::date;
